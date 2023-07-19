2e580eb5002a276c75fdc5c71c1892dc30d1df1f
pinak95 <pinak.thakar95@gmail.com>
2023-07-19 21:48:58 +0530

    New python files added as per the things I learnt

---
+++ Basics/messages.py
@@ -0,0 +1,14 @@
+# Various ways to write a hello world program
+print('Hello World')
+# Using a variable to write the welcome message
+message = 'Pinak'
+print('Hello World! I am ', message)
+# Using single quotes and double quotes to write the message
+print('Pinak\'s World')
+print("Pinak's World") # Single quotes and double quotes are the same but double quotes are more readable
+print("Pinak said \"Hello World\"")
+print('Pinak said "Hello World"') # Single quotes and double quotes are the same but single quotes are more readable
+# Using three quotes to write a multiple line message
+print("""Hello World! 
+I am Pinak and in this GitHub repository you would find me trying programming with python.
+I hope you like it.""")
